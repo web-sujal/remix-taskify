784ff22e446e3183482ff96845a334cbdea5cd28
Sujal <web.sujal@gmail.com>
2024-07-24 18:42:29 +0530

    refactor(create.tsx): add useTask hook

--- app/routes/create.tsx
+++ app/routes/create.tsx
@@ -9,14 +9,152 @@ import { useState } from "react";
 // import { json, redirect } from "@remix-run/node";
 
 import Header from "~/components/Header";
-import { isInvalidDueDate, getCurrentDate } from "~/utils";
-import { TaskType } from "~/types";
+import { isInvalidDueDate } from "~/utils";
+import { useTask } from "~/context/TaskContext";
 
 interface ErrorsType {
   title?: string;
   description?: string;
   dueDate?: string;
 }
+
+const Create = () => {
+  const { task, setTask } = useTask();
+  const [errors, setErrors] = useState<ErrorsType>({});
+
+  const navigation = useNavigation();
+  const navigate = useNavigate();
+
+  // const actionData = useActionData<typeof action>();
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const newErrors: ErrorsType = {};
+
+    if (task.title.length < 4) {
+      newErrors.title = "Title should be at least 4 characters";
+    }
+
+    if (task.description.length < 12) {
+      newErrors.description = "Description should be at least 12 characters";
+    }
+
+    if (isInvalidDueDate(task.dueDate)) {
+      newErrors.dueDate = "Due date cannot be a past date";
+    }
+
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+      return;
+    }
+
+    // Save to local storage
+    const storedTasks = window.localStorage.getItem("tasks");
+    const tasks = storedTasks ? JSON.parse(storedTasks) : [];
+    tasks.push(task);
+    window.localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    // Redirect to home page
+    navigate("/");
+  };
+
+  return (
+    <main className="h-screen w-screen flex flex-col items-center justify-start bg-emerald-50 p-10 gap-y-10">
+      <Header title="Create Task" description="What's on your mind!!!" />
+
+      <Form
+        method="post"
+        className="flex flex-col bg-emerald-100 rounded-lg p-10 items-start justify-start gap-y-6 max-w-lg w-full"
+        onSubmit={handleSubmit}
+      >
+        {/* title */}
+        <div className="flex w-full flex-col items-start justify-between gap-y-1">
+          <label className="text-lg font-bold text-emerald-900" htmlFor="title">
+            Title:
+          </label>
+          <input
+            className="input"
+            type="text"
+            name="title"
+            value={task.title}
+            onChange={(e) => setTask({ ...task, title: e.target.value })}
+            id="title"
+            placeholder="eg. buy groceries"
+            required
+          />
+
+          {/* error */}
+          {errors?.title && (
+            <em className="italic text-sm text-red-600">{errors.title}</em>
+          )}
+        </div>
+
+        {/* Description */}
+        <div className="flex w-full flex-col items-start justify-between gap-y-1">
+          <label
+            className="text-lg font-bold text-emerald-900"
+            htmlFor="description"
+          >
+            Description:
+          </label>
+          <textarea
+            rows={4}
+            className="input resize-none"
+            name="description"
+            id="description"
+            value={task.description}
+            onChange={(e) => setTask({ ...task, description: e.target.value })}
+            placeholder="eg. buy fruits..."
+            required
+          />
+
+          {/* error */}
+          {errors?.description && (
+            <em className="italic text-sm text-red-600">
+              {errors.description}
+            </em>
+          )}
+        </div>
+
+        {/* Due Date */}
+        <div className="flex w-full flex-col items-start justify-between gap-y-1">
+          <label
+            className="text-lg font-bold text-emerald-900"
+            htmlFor="dueDate"
+          >
+            Due Date:
+          </label>
+          <input
+            type="date"
+            value={task.dueDate}
+            name="dueDate"
+            onChange={(e) => setTask({ ...task, dueDate: e.target.value })}
+            id="dueDate"
+            className="input"
+            required
+          />
+
+          {/* error */}
+          {errors?.dueDate && (
+            <em className="italic text-sm text-red-600">{errors.dueDate}</em>
+          )}
+        </div>
+
+        {/* submit button */}
+        <button
+          type="submit"
+          disabled={navigation.state === "submitting"}
+          className="rounded-md bg-rose-700 text-white px-4 py-3 hover:bg-rose-800 disabled:cursor-not-allowed transition drop-shadow-xl text-extrabold w-full"
+        >
+          Create
+        </button>
+      </Form>
+    </main>
+  );
+};
+
+export default Create;
 
 // export const action = async ({ request }: ActionFunctionArgs) => {
 //   const formData = await request.formData();
@@ -61,153 +199,3 @@ interface ErrorsType {
 //   // Redirect to home if validation is successful
 //   return redirect("/");
 // };
-
-const Create = () => {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [dueDate, setDueDate] = useState(getCurrentDate);
-  const [errors, setErrors] = useState<ErrorsType>({});
-
-  const navigation = useNavigation();
-  const navigate = useNavigate();
-
-  // const actionData = useActionData<typeof action>();
-
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-
-    const newErrors: ErrorsType = {};
-
-    if (title.length < 4) {
-      newErrors.title = "Title should be at least 4 characters";
-    }
-
-    if (description.length < 12) {
-      newErrors.description = "Description should be at least 12 characters";
-    }
-
-    if (isInvalidDueDate(dueDate)) {
-      newErrors.dueDate = "Due date cannot be a past date";
-    }
-
-    if (Object.keys(newErrors).length > 0) {
-      setErrors(newErrors);
-      return;
-    }
-
-    // Create new task
-    const newTask: TaskType = {
-      id: Date.now(),
-      title,
-      description,
-      dueDate,
-      createdAt: new Date().toISOString(),
-      status: "pending",
-    };
-
-    // Save to local storage
-    const storedTasks = window.localStorage.getItem("tasks");
-    const tasks = storedTasks ? JSON.parse(storedTasks) : [];
-    tasks.push(newTask);
-    window.localStorage.setItem("tasks", JSON.stringify(tasks));
-
-    // Redirect to home page
-    navigate("/");
-  };
-
-  return (
-    <main className="h-screen w-screen flex flex-col items-center justify-start bg-emerald-50 p-10 gap-y-10">
-      <Header title="Create Task" description="What's on your mind!!!" />
-
-      <Form
-        method="post"
-        className="flex flex-col bg-emerald-100 rounded-lg p-10 items-start justify-start gap-y-6 max-w-lg w-full"
-        onSubmit={handleSubmit}
-      >
-        {/* title */}
-        <div className="flex w-full flex-col items-start justify-between gap-y-1">
-          <label className="text-lg font-bold text-emerald-900" htmlFor="title">
-            Title:
-          </label>
-          <input
-            className="input"
-            type="text"
-            name="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            id="title"
-            placeholder="eg. buy groceries"
-            required
-          />
-
-          {/* error */}
-          {errors?.title && (
-            <em className="italic text-sm text-red-600">{errors.title}</em>
-          )}
-        </div>
-
-        {/* Description */}
-        <div className="flex w-full flex-col items-start justify-between gap-y-1">
-          <label
-            className="text-lg font-bold text-emerald-900"
-            htmlFor="description"
-          >
-            Description:
-          </label>
-          <textarea
-            rows={4}
-            className="input resize-none"
-            name="description"
-            id="description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            placeholder="eg. buy fruits..."
-            required
-          />
-
-          {/* error */}
-          {errors?.description && (
-            <em className="italic text-sm text-red-600">
-              {errors.description}
-            </em>
-          )}
-        </div>
-
-        {/* Due Date */}
-        <div className="flex w-full flex-col items-start justify-between gap-y-1">
-          <label
-            className="text-lg font-bold text-emerald-900"
-            htmlFor="dueDate"
-          >
-            Due Date:
-          </label>
-          <input
-            type="date"
-            value={dueDate}
-            name="dueDate"
-            onChange={(e) => setDueDate(e.target.value)}
-            id="dueDate"
-            className="input"
-            required
-          />
-
-          {/* error */}
-          {errors?.dueDate && (
-            <em className="italic text-sm text-red-600">{errors.dueDate}</em>
-          )}
-        </div>
-
-        {/* submit button */}
-        <button
-          type="submit"
-          disabled={navigation.state === "submitting"}
-          className="rounded-md bg-rose-700 text-white px-4 py-3 hover:bg-rose-800 disabled:cursor-not-allowed transition drop-shadow-xl text-extrabold w-full"
-        >
-          Create
-        </button>
-      </Form>
-    </main>
-  );
-};
-
-export default Create;
