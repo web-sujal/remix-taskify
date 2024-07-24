c944c8af7fa22597e45959f36d527909fff257ec
Sujal <web.sujal@gmail.com>
2024-07-24 14:16:36 +0530

    add root layout

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,47 +2,31 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Taskify Home" },
+    {
+      name: "description",
+      content: "Welcome to taskify, where actions meet expectations.",
+    },
   ];
 };
 
 export default function Index() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="font-sans flex flex-col items-start justify-center bg-emerald-50 p-10 h-screen gap-y-10">
+      {/* header */}
+      <div className="flex w-full flex-col gap-y-3 pt-5 items-center justify-center">
+        <h1 className="text-6xl font-extrabold text-emerald-700">
+          Welcome to Taskify...
+        </h1>
+        <p className="text-gray-500 text-xl tracking-widest">
+          Where actions meet expectations!
+        </p>
+      </div>
+
+      {/* tasks list */}
+      <div className="w-full bg-emerald-100 rounded-md flex flex-col h-full items-center justify-start p-4">
+        Lorem Ipsum
+      </div>
     </div>
   );
 }
