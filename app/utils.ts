3d2a2b463d869b83a113605adac4738e31f15c53
Sujal <web.sujal@gmail.com>
2024-07-25 02:10:52 +0530

    add validateInputs util

--- app/utils.ts
+++ app/utils.ts
@@ -1,3 +1,5 @@
+import { ErrorsType } from "./types";
+
 export const isInvalidDueDate = (dueDate: string): boolean => {
   let currentDate = new Date().toISOString();
   currentDate = currentDate.slice(0, 10);
@@ -14,4 +16,30 @@ export const getCurrentDate = (): string => {
 
 export const formatDate = (date: string) => {
   return date.slice(0, 10);
+};
+
+export const validateInputs = (
+  title: string,
+  description: string,
+  dueDate: string
+) => {
+  const errors: ErrorsType = {};
+
+  if (title.length < 4) {
+    errors.title = "Title should be at least 4 characters";
+  }
+
+  if (description.length < 12) {
+    errors.description = "Description should be at least 12 characters";
+  }
+
+  if (isInvalidDueDate(dueDate)) {
+    errors.dueDate = "Due date cannot be a past date";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return errors;
+  }
+
+  return {};
 };
