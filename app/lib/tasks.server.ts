bfb0854b11e08421b30adc43c740f15e6f7a37c4
Sujal <web.sujal@gmail.com>
2024-07-28 16:26:44 +0530

    add getTask fn

--- app/lib/tasks.server.ts
+++ app/lib/tasks.server.ts
@@ -1,4 +1,4 @@
-import { createItem, readItems } from "@directus/sdk";
+import { createItem, readItem, readItems } from "@directus/sdk";
 
 import { TaskType } from "~/types";
 import directus from "./directus";
@@ -28,6 +28,16 @@ export const getTasks = async (): Promise<TaskType[]> => {
     );
 
     return tasks as TaskType[];
+  } catch (error) {
+    console.log((error as Error).message);
+    throw error;
+  }
+};
+
+export const getTask = async (taskId: string): Promise<TaskType> => {
+  try {
+    const task = await directus.request(readItem("Tasks", taskId));
+    return task;
   } catch (error) {
     console.log((error as Error).message);
     throw error;
